705ace034dc4781782deb0c1e97caad59f504b69
Juan Robledo <robledoc@gmail.com>
2023-05-14 04:38:56 -0400

    phase-1-building-simple-liker

--- main.js
+++ main.js
@@ -4,6 +4,45 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const emptyHearts = document.querySelectorAll('.like-glyph');
+emptyHearts.forEach(heart => {
+  heart.addEventListener('click', handleLike);
+});
+
+
+function handleLike(event) {
+  const heart = event.target;
+  mimicServerCall()
+    .then(() => {
+      
+      heart.classList.add('activated-heart');
+      heart.innerHTML = FULL_HEART;
+    })
+    .catch(() => {
+      
+      const errorModal = document.getElementById('modal');
+      errorModal.classList.remove('hidden');
+      const errorMessage = document.getElementById('modal-message');
+      errorMessage.textContent = 'Server Error. Please try again.';
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+}
+
+// Provided function to mimic server call
+function mimicServerCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const random = Math.random();
+      if (random < 0.5) {
+        resolve('success');
+      } else {
+        reject('error');
+      }
+    }, 300);
+  });
+}
 
 
 
